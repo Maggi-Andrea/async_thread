6686979290544beebeaf9a7914a83d7b87d2fc5a
Andrea Maggi <andrea@maggicontrols.com>
2020-01-08 17:11:37 +0100

    Test added

--- tests/test_asyncthread.py
+++ tests/test_asyncthread.py
@@ -1,24 +1,65 @@
-#!/usr/bin/env python
+'''
+Created on 19 nov 2019
 
-"""Tests for `asyncthread` package."""
+@author: Andrea
+'''
+import unittest
 
-import pytest
+import asyncio
 
-
-from asyncthread import asyncthread
-
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+import time
+from asyncthread import AsyncThread
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+async def task_service():
+  while True:
+    await asyncio.sleep(1.0)
+  return True
+
+class Test(unittest.TestCase):
+
+  def test_AsyncThread(self):
+    
+    loop = asyncio.get_event_loop()
+    thread = AsyncThread()
+    thread.daemon = True
+    thread.start()
+    thread.join(1)
+    
+    assert thread.is_alive()
+    assert thread.loop.is_running()
+    assert not thread.loop.is_closed()
+    
+    assert loop is not thread.loop
+    assert not loop.is_running()
+    assert not loop.is_closed()
+    
+    future = thread.run_coroutine_threadsafe(task_service())
+    
+    time.sleep(0.1) #Give time to OS to switch to the asynch thread
+    
+    assert future.running()
+    assert not future.done()
+    
+    assert thread.is_alive()
+    assert thread.loop.is_running()
+    assert not thread.loop.is_closed()
+    
+    assert loop is not thread.loop
+    assert not loop.is_running()
+    assert not loop.is_closed()
+
+    thread.stop()
+    
+    assert future.running()
+    assert not future.done()
+    
+    assert not thread.is_alive()
+    assert not thread.loop.is_running()
+    assert thread.loop.is_closed()
+    
+    
+
+if __name__ == "__main__":
+  #import sys;sys.argv = ['', 'Test.testName']
+  unittest.main()
